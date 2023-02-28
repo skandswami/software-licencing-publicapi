ea92a2f1fba049c5e6ded9643bbe72f326481b41
skandswami <59348205+skandswami@users.noreply.github.com>
2023-02-28 16:43:31 +0100

    Added request model validator using FluentValidation

--- software-licencing/RegisterationService/Models/RegisterSoftwareRequestModel.cs
+++ software-licencing/RegisterationService/Models/RegisterSoftwareRequestModel.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using FluentValidation;
+
 namespace RegisterationService.Models;
 
 public record RegisterSoftwareRequestModel(string CompanyName, string ContactPerson, string Email, string LicenceKey);
+
+public class RegisterSoftwareRequestModelValidator : AbstractValidator<RegisterSoftwareRequestModel>
+{
+    public RegisterSoftwareRequestModelValidator()
+    {
+        RuleFor(m => m.Email).EmailAddress().When(x => x.Email is not null);
+    }
+}
