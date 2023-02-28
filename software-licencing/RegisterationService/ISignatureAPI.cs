002269a28407a12741f58cef8f21f9709c1899d3
skandswami <59348205+skandswami@users.noreply.github.com>
2023-02-28 16:43:09 +0100

    Adding Refit client for SignatureAPI calling

---
+++ software-licencing/RegisterationService/ISignatureAPI.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Refit;
+using RegisterationService.Models;
+namespace RegisterationService;
+
+public interface ISignatureAPI
+{
+	[Post("/Signature")]
+	Task SignSoftware([Body] SignatureRequestModel model);
+}
+
--- software-licencing/RegisterationService/Program.cs
+++ software-licencing/RegisterationService/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Refit;
+using RegisterationService;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -7,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRefitClient<ISignatureAPI>().ConfigureHttpClient(c => 
+{
+    // TODO : The value of the url should come from the configurations which should be stored in some sort of vault.
+    c.BaseAddress = new Uri(builder.Configuration.GetValue<string>("https://localhost:7221"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
